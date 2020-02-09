72d3e0712ba6bb298037e1c6e7ff3eb7eda1ec84
Tears <i@ltteam.cn>
2020-02-09 13:42:24 +0800

    快速提交

--- main.go
+++ main.go
@@ -4,73 +4,18 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/micro/go-micro"
-	"sync"
-	// Import the generated protobuf code
 	pb "github.com/HuiguoRose/shippy-service-consignment/proto/consignment"
+	vesselProto "github.com/HuiguoRose/shippy-service-vessel/proto/vessel"
+	"github.com/micro/go-micro"
+	"log"
+	"os"
 )
 
-type repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-
-// Repository - Dummy repository, this simulates the use of a datastore
-// of some kind. We'll replace this with a real implementation later on.
-type Repository struct {
-	mu           sync.RWMutex
-	consignments []*pb.Consignment
-}
-
-// Create a new consignment
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	repo.mu.Lock()
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	repo.mu.Unlock()
-	return consignment, nil
-}
-
-// GetAll consignments
-func (repo *Repository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-// Service should implement all of the methods to satisfy the service
-// we defined in our protobuf definition. You can check the interface
-// in the generated code itself for the exact method signatures etc
-// to give you a better idea.
-type service struct {
-	repo repository
-}
-
-// CreateConsignment - we created just one method on our service,
-// which is a create method, which takes a context and a request as an
-// argument, these are handled by the gRPC server.
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-
-	// Save our consignment
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	// Return matching the `Response` message we created in our
-	// protobuf definition.
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-// GetConsignments -
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
+const (
+	defaultHost = "datastore:27017"
+)
 
 func main() {
-	repo := &Repository{}
 
 	// Create a new service. Optionally include some options here.
 	srv := micro.NewService(
@@ -82,8 +27,27 @@ func main() {
 	// Init will parse the command line flags.
 	srv.Init()
 
+	uri := os.Getenv("DB_HOST")
+	if uri == "" {
+		uri = defaultHost
+	}
+
+	client, err := CreateClient(context.Background(), uri, 0)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer client.Disconnect(context.Background())
+
+	consignmentCollection := client.Database("shippy").Collection("consignments")
+
+	repository := &MongoRepository{consignmentCollection}
+
+	vesselClient := vesselProto.NewVesselServiceClient("shippy.service.vessel", srv.Client())
 	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	pb.RegisterShippingServiceHandler(srv.Server(), &handler{
+		repository:   repository,
+		vesselClient: vesselClient,
+	})
 
 	// Run the server
 	if err := srv.Run(); err != nil {
